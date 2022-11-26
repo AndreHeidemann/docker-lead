3a6cdf41f36cacec9aa7145d98bddfc9402b276c
meendreak <50605005+meendreak@users.noreply.github.com>
2022-11-26 14:47:46 -0300

    Adicionado compose

--- www/api/model/db.php
+++ www/api/model/db.php
@@ -5,10 +5,10 @@ class Database {
 public $hostname, $dbname, $username, $password, $conn;
 
 function __construct() {
-    $this->host_name = "192.168.3.6";
-    $this->dbname = "root";
+    $this->host_name = "mysql";
+    $this->dbname = "LEADS_DB";
     $this->username = "root";
-    $this->password = "LEADS_DB";
+    $this->password = "root";
     try {
 
         $this->conn = new PDO("mysql:host=$this->host_name;dbname=$this->dbname", $this->username, $this->password);
